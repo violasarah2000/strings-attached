e0247f0c27b38481db47eafbad30ef0b7ee730fc
John Louie Binas <jlouie.binas@neworientalclub88.com>
2018-12-26 01:25:16 +0800

    Revert back to localhost

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -8,7 +8,7 @@ const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
 
-const apiUrl = "http://stringsattachedtx.com:3000/bookings";
+const apiUrl = "http://127.0.0.1:3000/bookings";
 
 @Injectable({
   providedIn: 'root'
